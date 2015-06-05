84e2fe9bd25f93e0dc2218642054c66a519eb86b
Julian Moosmann <moosmann@kth.se>
2015-06-05 17:01:43 +0200

    Initial commit.

---
+++ criticality/CTFanalysis-originalScripts-thetastepFixed/SavePlotSequence.m
@@ -0,0 +1,14 @@
+%% run CTFanalysis.m before, BUT ORIGINAL VERSION
+
+xfull = 1:size(y, 1);
+xroi = 25:180;
+nmax = size(y, 2);
+for nn = 1:nmax
+    plot(xfull, y(:,nn), '.b', ...
+        xroi, cf{nn}(xroi), '-r', ...
+        cfMinPos(nn), cfMinVal(nn), 'og')
+    axis tight
+    legend({sprintf('%u of %u\n$\\phi_{max} = $ %4.02g', nn, nmax, MaxPhaseShift(nn))},'Interpreter','latex')
+    saveas(gcf, sprintf('./plots/plot_%03u', nn), 'png')
+    %pause(10/512)
+end
---
+++ criticality/CTFanalysis-originalScripts-thetastepFixed/figOL_PaddedLenaTestPattern.m
@@ -0,0 +1,33 @@
+%Prior to execution the scrip 'FitCritExpoScript.m' needs to be run or the
+%data 'CTFanlaysis2.mat' needs to be loaded.
+
+ca
+% Create figure
+figure1 = figure('Name','Padded Lena test pattern');
+colormap('gray');
+%set(gcf,'position',[0 0, 900 900])
+set(gcf,'defaulttextinterpreter','none')
+
+% Create axes
+axes1 = axes('Parent',figure1,'YTick',[0 200 400 600 800 1000],...
+    'YAxisLocation','right',...
+    'YDir','reverse',...
+    'XTick',[0 200 400 600 800 1000],...
+    'TickDir','out',...
+    'Layer','top',...
+    'FontSize',18,...
+    'DataAspectRatio',[1 1 1],...
+    'CLim',[0 0.01]);
+
+xlim(axes1,[1 1024]);
+ylim(axes1,[1 1024]);
+box(axes1,'on');
+hold(axes1,'all');
+
+% Create image
+image(phase,'Parent',axes1,'CDataMapping','scaled');
+
+saveas(gcf,'./figs/Fig-1a.eps','epsc2')
+set(gcf,'PaperPositionMode','Auto') 
+saveas(gcf,'./figs/Fig-1a_nonquadr.eps','epsc2')
+saveas(gcf,'./figs/LenaTestPatternPadded_(Fig-1a).eps','epsc2')
